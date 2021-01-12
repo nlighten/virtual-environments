25e49031b9a029308c9459b61c1e36f282512c6f
Richard <richard.swart@nlighten.nl>
2021-01-12 14:14:25 +0100

    debug

--- images/linux/scripts/installers/git.sh
+++ images/linux/scripts/installers/git.sh
@@ -3,6 +3,7 @@
 ##  File:  git.sh
 ##  Desc:  Installs Git
 ################################################################################
+set -xv
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
